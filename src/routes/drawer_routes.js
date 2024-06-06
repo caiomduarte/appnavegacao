318b00f29b36776620092557a3df2e588e567146
Caio Malheiros <md.caio@yahoo.com.br>
2024-06-05 21:10:43 -0300

    Drawer

--- src/routes/drawer_routes.js
+++ src/routes/drawer_routes.js
@@ -1,102 +1,28 @@
+//1 passo - importar um componenete createDrawerNavigatior
+import {createDrawerNavigator} from '@react-navigation/drawer'
 
-import { View, Text, Image, StyleSheet } from 'react-native';
+//2 passo - importar as telas que vão ser navegadas
+import Home from '../pages/home';
+import Feed from '../pages/feed';
 
-//1 passo - importar o createDrawerNavigator
-import { DrawerContentScrollView, DrawerItem, createDrawerNavigator } from '@react-navigation/drawer'
-
-//2 passo - Importar o arquivo de rotas de abas
-import TabRoutes from './tab_routes';
-
-//3 passo - criar um Drawer
+import {FontAwesome, Octicons} from '@expo/vector-icons';
+//passo 3 - criar uma variavel para 
 const Drawer = createDrawerNavigator();
 
-//Depois de funcionar - importar um pacote de icones
-import { Feather } from '@expo/vector-icons';
-
-//Importar as telas
-import Home from '../pages/Home';
-import Feed from '../pages/Feed';
-
-import CustomDrawer from '../components/CustomDrawer';
-
-export default function DrawerRoutes() {
-    return (
-        <Drawer.Navigator drawerContent={props => {
-            return (
-                <DrawerContentScrollView style={styles.container}>
-                    <View>
-                        <View style={styles.userArea}>
-                            <Image source={{ uri: 'https://randomuser.me/api/portraits/men/27.jpg' }} style={styles.user} />
-                            <Text style={styles.nome}>Caio Malheiros</Text>
-                            <Text style={styles.email}>md.caio@yahoo.com.br</Text>
-                        </View>
-                    </View>
-
-                    <DrawerItem
-                        label={'Inicio'}
-                        onPress={() => {
-                            props.navigation.navigate('home')
-                        }}
-
-                    />
-
-                    <DrawerItem
-                        label={'Feed'}
-                        onPress={() => {
-                            props.navigation.navigate('feed')
-                        }}
-
-                    />
-                </DrawerContentScrollView>
-            )
-
+export default function DrawerRoutes(){
+    return(
+        <Drawer.Navigator screenOptions={{
+            headerShown: true,
         }}>
             <Drawer.Screen name="home" component={Home}
-                options={{
-                    drawerIcon: () => <Feather name="home" size={20} color="green" />,
-                    drawerLabel: 'Inicio',
-                }}
+            options={{
+                drawerLabel: 'Inicio', 
+                drawerActiveBackgroudColor: 'Red',
+                drawerIcon:() => <FontAwesome name="home" size={24} color="black"/>,
+            }}
             />
-
-            <Drawer.Screen name="feed" component={Feed}
-                options={{
-                    drawerIcon: () => <Feather name="home" size={20} color="white" />,
-                    drawerLabel: 'Feed'
-                }}
-            />
-
-
-        </Drawer.Navigator>
+            <Drawer.Screen name="feed" component={Feed}/>
+             </Drawer.Navigator>
+       
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor:'red'
-    },
-
-    userArea: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 15,
-        marginLeft: 10,
-        marginBottom: 10
-    },
-
-    user: {
-        width: 55,
-        height: 55,
-        borderRadius: 50
-    },
-
-    nome: {
-        marginTop: 10,
-        fontSize: 18,
-        fontWeight: 'bold'
-    },
-
-    email: {
-        fontSize: 15
-    }
-})
